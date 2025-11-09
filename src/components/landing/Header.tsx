b59375fbbb28364aa6186b0433ebcfdce9eb58d8
Prateek Mittal <134774647+prateek15000@users.noreply.github.com>
2025-11-10 02:26:08 +0530

    Update Header.tsx

--- src/components/landing/Header.tsx
+++ src/components/landing/Header.tsx
@@ -9,7 +9,7 @@ function Header() {
       <div className="max-w-6xl mx-auto flex justify-between items-center">
         <Link href="/" className="flex items-center gap-2">
           <Image src={"/logo.png"} alt="DentWise Logo" width={32} height={32} className="w-11" />
-          <span className="font-semibold text-lg">DentWise</span>
+          <span className="font-semibold text-lg">MedWise</span>
         </Link>
 
         <div className="hidden md:flex items-center gap-8">
@@ -27,7 +27,7 @@ function Header() {
         <div className="flex items-center gap-3">
           <SignInButton mode="modal">
             <Button variant={"ghost"} size={"sm"}>
-              Login 
+              Login
             </Button>
           </SignInButton>
           <SignUpButton mode="modal">
@@ -39,3 +39,4 @@ function Header() {
   );
 }
 export default Header;
+
